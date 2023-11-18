bfb4aef620df75af34538203d0707062d945a981
Isa147R <131714039+Isa147R@users.noreply.github.com>
2023-11-18 12:13:41 -0500

    Create Contacto.cpp

---
+++ Contacto.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// Definición de la estructura de un contacto
+struct Contacto {
+    std::string nombre;
+    std::string telefono;
+    std::string redesSociales;
+    std::string icono;
+    std::string direccion;
+};
